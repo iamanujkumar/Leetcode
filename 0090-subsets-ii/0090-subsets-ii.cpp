ed2d30c1c60253286d8a9a29f0f61daedff843c5
Anuj Kumar <147647340+iamanujkumar@users.noreply.github.com>
2024-07-10 16:27:36 +0530

    Time: 4 ms (48.62%), Space: 8.7 MB (82.98%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-void fun(vector<int>nums, int n,int i,vector<int>v,vector<vector<int>>&ans){
-      if(i==n){
-          ans.push_back(v);
-          return;
-      }
-      v.push_back(nums[i]);
-      fun(nums,n,i+1,v,ans);
-      v.pop_back();
-      while (i + 1 < n && nums[i] == nums[i + 1]) i++;
-        fun(nums, n, i + 1, v, ans);
-      
-  }
+    void fun(vector<int>& nums, int i, vector<int>& v, vector<vector<int>>& ans) {
+        ans.push_back(v);
+        for (int j = i; j < nums.size(); ++j) {
+            if (j > i && nums[j] == nums[j - 1]) continue; // skip duplicates
+            v.push_back(nums[j]);
+            fun(nums, j + 1, v, ans);
+            v.pop_back();
+        }
+    }
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
-        vector<int>v;
-        fun(nums,nums.size(),0,v,ans);
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> v;
+        fun(nums, 0, v, ans);
         return ans;
     }
 };
