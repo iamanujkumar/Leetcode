1513602445347e19bd70ee610528d7134ec037b0
Anuj Kumar <147647340+iamanujkumar@users.noreply.github.com>
2024-07-12 10:57:41 +0530

    Time: 0 ms (100%), Space: 7.5 MB (27.28%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,28 +1,11 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        string s1="";
-        string s2="";
-        while(start>0){
-            s1+=(start&1)?'1':'0';
-
-            start>>=1;
-        }
-        while(goal>0){
-            s2+=(goal&1)?'1':'0';
-            goal>>=1;
-        }
-        while(s1.size()<s2.size()){
-            s1+='0';
-        }
-        while(s2.size()<s1.size()){
-            s2+='0';
-        }
+        int xorres= start^goal;
         int count=0;
-        for(size_t i=0;i<s1.size();++i){
-            if(s1[i]!=s2[i]){
-                count++;
-            }
+        while(xorres>0){
+            count += xorres&1;
+            xorres>>=1;
         }
         return count;
     }
