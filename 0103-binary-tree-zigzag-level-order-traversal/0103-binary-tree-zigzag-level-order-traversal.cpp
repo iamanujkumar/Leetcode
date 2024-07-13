c2dad1fde757babaf4c221f7e645d4f6b9e7cbeb
Anuj Kumar <147647340+iamanujkumar@users.noreply.github.com>
2024-07-13 11:53:01 +0530

    Time: 0 ms (100%), Space: 13.4 MB (5.72%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,25 +12,24 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        if (!root) return ans;
-        
-        queue<TreeNode*> q;
+        vector<vector<int>>ans;
+        if(root==NULL) return ans;
+        queue<TreeNode*>q;
         q.push(root);
-        bool leftToRight = true;
-
-        while (!q.empty()) {
-            int n = q.size();
-            vector<int> v(n);
-            for (int i = 0; i < n; ++i) {
-                TreeNode* temp = q.front();
+        int count=0;
+        while(!q.empty()){
+            int n=q.size();
+            vector<int>v;
+            count++;
+            for(int i=0;i<n;i++){
+                TreeNode *temp=q.front();
                 q.pop();
-                int index = leftToRight ? i : n - 1 - i;
-                v[index] = temp->val;
-                if (temp->left) q.push(temp->left);
-                if (temp->right) q.push(temp->right);
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+                v.push_back(temp->val);
+                
             }
-            leftToRight = !leftToRight;
+            if(count%2==0) reverse(v.begin(),v.end());
             ans.push_back(v);
         }
         return ans;
