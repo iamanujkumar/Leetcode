916d7b7f086c0f5ac1e63d554404e491aff37f2f
Anuj Kumar <147647340+iamanujkumar@users.noreply.github.com>
2024-07-26 17:30:21 +0530

    Time: 0 ms (100%), Space: 9.5 MB (43.17%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-int fun(vector<int>&v){
-    int n=v.size();
-    int prev2=0;
-    int prev1=v[0];
-    for(int i=1;i<n;i++){
-        int incl = prev2+v[i];
-        int exc = prev1+0;
-        int ans = max(incl,exc);
-        prev2=prev1;
-        prev1=ans;
-    }
-    return prev1;
+int fun(vector<int>&nums,int start,int end){
+        if (end - start == 1) return nums[start];
+        
+        int prev2 = nums[start];
+        int prev1 = max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i < end; i++) {
+            int curr = max(nums[i] + prev2, prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
 }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1){
-            return nums[0];
-        }
-        vector<int>first,last;
-        for(int i=0;i<n;i++){
-            if(i!=n-1){
-                first.push_back(nums[i]);
-            }
-            if(i!=0){
-                last.push_back(nums[i]);
-            }
-        }
-        return max(fun(first),fun(last));
+         int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
+       int inc = fun(nums,0,nums.size()-1);
+       int rej = fun(nums,1,nums.size());
+       return max(inc,rej);
     }
 };
