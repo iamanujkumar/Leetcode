fc5e28f01988de8c67a6840b12dcff9e4fbf0328
Anuj Kumar <147647340+iamanujkumar@users.noreply.github.com>
2024-08-30 00:43:48 +0530

    Time: 9 ms (61.91%), Space: 14.5 MB (38.56%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
         int sum = 0;
-        for (int num : nums) {
-            sum += num;
+        
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
         }
-        if ((target + sum) % 2 != 0 || (target + sum) < 0) {
+
+        // Check if the problem is feasible
+        if (sum < abs(target) || (sum + target) % 2 != 0) {
             return 0;
         }
 
-        int s = (target + sum) / 2;
-        int n = nums.size();
-        vector<int> dp(s + 1, 0);
-        dp[0] = 1;
+        int val = (sum + target) / 2;
 
-        for (int num : nums) {
-            for (int j = s; j >= num; --j) {
-                dp[j] += dp[j - num];
+        // If val is negative or too large, return 0
+        if (val < 0 || val > sum) {
+            return 0;
+        }
+
+        vector<vector<int>> dp(n + 1, vector<int>(val + 1, 0));
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= val; j++) {
+                if (nums[i-1] <= j) {
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j - nums[i-1]];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
 
-        return dp[s];
+        return dp[n][val];
     }
 };
