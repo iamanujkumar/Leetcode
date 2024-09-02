515c62a9e556e2949f34012fab2dfe049c9f25d4
Anuj Kumar <147647340+iamanujkumar@users.noreply.github.com>
2024-09-03 00:34:25 +0530

    Time: 8 ms (63.12%), Space: 20.3 MB (30.82%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -11,8 +11,20 @@ int fun(int n,int k,vector<int>&v,vector<vector<int>>&dp){
     else return dp[n][k] = fun(n-1,k,v,dp);
 }
     int change(int amount, vector<int>& coins) {
-         int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        return fun(n,amount,coins,dp);
+    int n = coins.size();
+    vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
+    for (int i = 0; i <= n; i++) {
+        dp[i][0] = 1;
     }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= amount; j++) {
+            if (coins[i - 1] <= j)
+                dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
+    }
+
+    return dp[n][amount];
+}
 };
