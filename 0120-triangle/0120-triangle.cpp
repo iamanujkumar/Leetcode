16bef0de29bbed6cbee7679d2b793b47ee3068b1
Anuj Kumar <147647340+iamanujkumar@users.noreply.github.com>
2025-09-25 23:13:41 +0530

    Time: 0 ms (100%), Space: 12.7 MB (81.37%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
-
-int fun(int m,int n,vector<vector<int>>&triangle,vector<vector<int>>&dp){
-    if (m < 0 || n < 0 || n > m) {
-            return INT_MAX; // Out of bounds case
-        }
-    if(m==0 && n==0){
-        return triangle[0][0];
-    }
-    if(dp[m][n]!=-1){
-        return dp[m][n];
-    }
-    int up = INT_MAX, left = INT_MAX;
-    if(m>0) up = fun(m-1,n,triangle,dp);
-    if(n>0) left = fun(m-1,n-1,triangle,dp);
-
-    return dp[m][n] = triangle[m][n]+min(up,left);
-}
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m=triangle.size();
-        vector<vector<int>>dp(m,vector<int>(m,-1));
-        int ans=INT_MAX;
-        for(int i=0;i<m;++i){
-             ans = min(ans, fun(m-1,i,triangle,dp));
+        int n = triangle.size();
+        vector<int> dp = triangle[n-1];  
+        for(int i = n-2; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                dp[j] = triangle[i][j] + min(dp[j], dp[j+1]);
+            }
         }
-     return ans;   
+        return dp[0];
     }
 };
