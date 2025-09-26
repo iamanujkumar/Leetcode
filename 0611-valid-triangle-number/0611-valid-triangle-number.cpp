5b4090dab328806116edad0910df6b90b2949353
Anuj Kumar <147647340+iamanujkumar@users.noreply.github.com>
2025-09-26 19:30:48 +0530

    Time: 187 ms (15%), Space: 16.6 MB (42.88%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
+    int binary_search(int l,int r,vector<int>&nums,int sum){
+        int k = -1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(nums[mid]<sum){
+                k = mid;
+                l = mid+1;
+            }
+            else r = mid-1;
+        }
+        return k;
+    }
     int triangleNumber(vector<int>& nums) {
         int count=0;
         if(nums.size()<3) return 0;
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-2;i++){
+            if(nums[i]==0) continue;
             for(int j=i+1;j<nums.size()-1;j++){
-                if(nums[i]!=0 && nums[j]!=0){
-                    int sum = nums[i]+nums[j];
-                    for(int k=j+1;k<nums.size();k++){
-                       if(sum>nums[k]) count++;
-                    }
-                }
+                int sum=nums[i]+nums[j];
+                int k = binary_search(j+1,nums.size()-1,nums,sum);
+                if(k!=-1) count+=(k-j);
             }
         }
         return count;
